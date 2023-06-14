ec7abab7ca83d95f914d0d20af5bd705d1a888d7
Santiago Nicolás Agüero Urquiza <santiagonaguero@gmail.com>
2023-06-13 21:45:21 -0300

    123456

--- src/index.js
+++ src/index.js
@@ -3,19 +3,23 @@ const connection = require("../db");
 
 const router = Router();
 
-/* router.get('/:id/passengers', checkIn);   boarding_pass   boarding_pass.seat_id  passenger
-
+/* "passenger_id": 4, 4
 `SELECT * FROM boarding_pass 
-    JOIN passenger 
+    INNER JOIN passenger 
     ON boarding_pass.passenger_id = passenger.passenger_id
-;
+    INNER JOIN purchase 
+    ON boarding_pass.purchase_id = purchase.purchase_id
+    INNER JOIN seat 
+    ON boarding_pass.seat_id = seat.seat_id
+    INNER JOIN seat_type 
+    ON boarding_pass.seat_type_id = seat_type.seat_type_id
+    INNER JOIN flight 
+    ON boarding_pass.flight_id = flight.flight_id
+    INNER JOIN airplane 
+    ON seat.airplane_id = airplane.airplane_id
+    WHERE age<30 
+    ;   
 `
-
-
-
-    INNER JOIN seat_type_id 
-    ON boarding_pass.seat_type_id = purchase.seat_type_id
-    
 */
 
 router.get("/:id", (req,res) => {
@@ -33,11 +37,13 @@ router.get("/:id", (req,res) => {
     INNER JOIN flight 
     ON boarding_pass.flight_id = flight.flight_id
     INNER JOIN airplane 
-    ON seat.airplane_id = airplane.airplane_id;
+    ON seat.airplane_id = airplane.airplane_id
+    ;   
 `
 
 
     connection.query (sql_query, (err,result)=> {
+      console.log(sql_query.length);
        if (err) throw err;
        res.send(result);
     }
