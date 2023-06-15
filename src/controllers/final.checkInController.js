2403a2c0f4e507923e3065d60d39b4be2b3971ca
Santiago Nicolás Agüero Urquiza <santiagonaguero@gmail.com>
2023-06-15 01:41:07 -0300

    getminors working

--- src/controllers/final.checkInController.js
+++ src/controllers/final.checkInController.js
@@ -3,16 +3,19 @@ const getSeats = require("../utils/getData/getSeats");
 const getBoardingPass = require("../utils/getData/getBoardingPass");
 const getFlight = require("../utils/getData/getFlight");
 const getPassengers = require("../utils/getData/getPassengers");
-const getMinors = require("../utils/getMinors");
 
 const checkIn = async (req, res) => {
   const { id } = req.params;
   const flightId = parseInt(id);
 
-  const allMinors = await getMinors(flightId);
-  res.status(200).send(allMinors)
 
+  
 
+  let allFlights = await getFlight(flightId);
+  const allPassenger = await getPassengers(flightId);
+  allFlights[0].passengers = allPassenger;
+  res.status(200).send(allFlights)
+  
 };
 
 module.exports = checkIn;
---
+++ src/utils/getMinors.js
@@ -0,0 +1,29 @@
+const connection = require("../../db");
+
+const getMinors = (flightId) => {
+    return new Promise((resolve, reject) => {
+        connection.query(
+            `
+            SELECT *
+            FROM boarding_pass
+            INNER JOIN flight 
+            ON boarding_pass.flight_id = flight.flight_id
+            INNER JOIN passenger 
+            ON boarding_pass.passenger_id = passenger.passenger_id
+                 WHERE age < 18 AND flight.flight_id = ${flightId}
+          `,
+          (err, result) => {
+            if (err) {
+              reject(err);
+            } else {
+                console.log(result.length);
+              resolve(result);
+            }
+          }
+        );
+      });
+   };
+   
+   module.exports = getMinors ;
+
+
