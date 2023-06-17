ddd7925662556fd469f979e501971b5279e92403
Santiago Nicolás Agüero Urquiza <santiagonaguero@gmail.com>
2023-06-17 02:53:24 -0300

    cleaning

--- src/controllers/checkInController.js
+++ src/controllers/checkInController.js
@@ -1,17 +1,58 @@
+const assignSeats = require("../utils/AsignarAsientos/getSeats");
 const getFlight = require("../utils/getData/getFlight");
 const getPassengers = require("../utils/getData/getPassengers");
+const {findPassengerWithMinorsByPurchaseId, filterPassengersWithNoMinors} = require("../utils/getSeatsData/getMinorGuardian");
+const getMinors = require("../utils/getSeatsData/getMinors");
 const getSeats = require("../utils/getSeatsData/getSeats");
 
-
-const checkIn = async (req, res) => {
+const checkInController = async (req, res) => {
   const { id } = req.params;
   const flightId = parseInt(id);
+  try {
+    if (flightId !== 1 && flightId !== 2 && flightId !== 3 && flightId !== 4) {
+     throw new Error('flight not found');
+    }
+//get passengers and group them
+const flight = await getFlight(flightId); 
+  const passengers = await getPassengers(flightId); 
+  const minors = await getMinors(flightId); 
+  const GroupswithMinorsByPurchaseID = await findPassengerWithMinorsByPurchaseId(minors, passengers); 
+  const getGroupsWithNoMinors = await filterPassengersWithNoMinors(minors, passengers);
 
+// GET SEATS 
+  const allSeats = await getSeats(flightId); 
 
-  const seats = await getSeats(flightId);
-  res.status(200).send(seats)
+//assign seats 
+const passengersAssignedSeats = await assignSeats(allSeats, GroupswithMinorsByPurchaseID, getGroupsWithNoMinors);
 
+ return res.json({
+  code: 200,
+  data: {
+   flightId: flight[0].flight_id,
+   takeoffDateTime: flight[0].takeoff_date_time,
+   takeoffAirport: flight[0].takeoff_airport,
+   landingDateTime: flight[0].landing_date_time,
+   landingAirport: flight[0].landing_airport,
+   airplaneId: flight[0].airplane_id,
+   passengers: passengersAssignedSeats,
+  },
+ }); 
+
+} catch (error) {
+  console.error(error);
+  if (error.message === 'flight not found') {
+   return res.status(404).json({
+    code: 404,
+    data: {},
+   });
+  }
+  return res.status(400).json({
+   code: 400,
+   errors: 'could not connect to db',
+  });
+
+ }
 
 };
 
-module.exports = checkIn;
+module.exports = checkInController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 const {Router} = require("express");
-const checkIn = require("../controllers/checkInController");
-const asignSeatToMinor = require("../controllers/asignSeatToMinor");
+const checkInController = require("../controllers/checkInController");
 
 
 const router = Router();
@@ -8,6 +7,6 @@ const router = Router();
 
 /* router.get('/:id', asignSeatToMinor); */  //este es el bueno
 
-router.get('/:id', asignSeatToMinor);
+router.get('/:id/passengers', checkInController);
 
 module.exports = router;
